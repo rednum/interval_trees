be85bbdd12ef4a20c86fd3274e52b9809c915911
Marcin Fatyga <fatygamarcin@gmail.com>
2015-09-12 22:26:02 +0200

    Added bigger test

--- tests/segmentpoint.rs
+++ tests/segmentpoint.rs
@@ -90,3 +90,41 @@ fn small_queries_3() {
     }
 }
 
+#[test]
+fn test_big() {
+    let mut t = SegmentPointTree::new(0, 1_000_000, 0, Box::new(|x: &i64, y: &i64| x + y));
+    assert_eq!(t.bounds(), (0, 1_000_000));
+    assert_eq!(t.query(-1), None);
+    assert_eq!(t.query(2_000_000), None);
+    t.insert(0, 1000, 1000);
+    t.insert(1, 1000, 1000);
+    t.insert(2, 1000, 1000);
+    t.insert(3, 1000, 1000);
+    t.insert(4, 1000, 1000);
+
+    assert_eq!(t.query(0), Some(1000));
+    assert_eq!(t.query(1), Some(2000));
+    assert_eq!(t.query(2), Some(3000));
+    assert_eq!(t.query(3), Some(4000));
+    assert_eq!(t.query(4), Some(5000));
+    assert_eq!(t.query(100), Some(5000));
+    assert_eq!(t.query(1000), Some(5000));
+
+    t.insert(2000, 10_000, 2000);
+    t.insert(2000, 20_000, 2000);
+    t.insert(120_444, 120_444, 8);
+    t.insert(3000, 100_000, 2000);
+    t.insert(3000, 200_000, 2000);
+
+    assert_eq!(t.query(2000), Some(4000));
+    assert_eq!(t.query(3000), Some(8000));
+    assert_eq!(t.query(100_000), Some(4000));
+    assert_eq!(t.query(120_443), Some(2000));
+    assert_eq!(t.query(120_444), Some(2008));
+    assert_eq!(t.query(120_445), Some(2000));
+    assert_eq!(t.query(200_000), Some(2000));
+
+    assert_eq!(t.query(0), Some(1000));
+    assert_eq!(t.query(1_000_000), Some(0));
+    assert_eq!(t.query(1_000_001), None);
+}
